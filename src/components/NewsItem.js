6be15a9f3d38524a5bca8a43377bbf0ff5ca624b
anasalam-xyz <anasalam.xyz@gmail.com>
2025-10-14 22:43:07 +0530

    iterating over articles to show NewsItem

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-    return (
-        <div className="card" style={{width: "18rem"}}>
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
+        let {title, description, imageURL,  newsURL} = this.props;
+        return (
+            <div className="card" style={{width: "18rem"}}>
+                <img src={imageURL} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href={newsURL} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                </div>
+            </div>  
         )
     }
 }
